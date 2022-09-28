8f40cbea277f1c2a885236f2d3420be3d74f7349
CISSE-Mai <cmai7262@gmail.com>
2022-09-28 11:30:51 +0000

    second

---
+++ week2/day1/Dally challenge/exercice1.js
@@ -0,0 +1,15 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+let fruit=fruits.slice(1);
+console.log(fruit);
+
+fruit.sort();
+console.log(fruit);
+
+fruit.push("kiwi");
+console.log(fruit);
+
+fruit.splice(0,1);
+console.log(fruit);
+
+fruit.reverse();
+console.log(fruit);
